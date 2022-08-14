dc1b5a2706a5a3df90428321725b967701989d0a
kimsukiflcl <31473466+kimsukiflcl@users.noreply.github.com>
2022-08-14 12:34:19 +0530

    flow001.cpp
    flow001 from cpp gives error in test cases but the code is right
    take no of test cases and print sum of two int numbers

---
+++ flow001.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+
+    int T;
+    cin >> T;
+
+    for(int i=0; i<=T; ++i) {
+        int a, b;
+        cin >> a >> b;
+
+        int ans = a+b;
+        cout << ans << endl;
+
+    }
+
+
+    return 0;
+}
