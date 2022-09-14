b3ecca60db5af44942a0e07fcc95df0701d6a8db
kimsukiflcl <31473466+kimsukiflcl@users.noreply.github.com>
2022-09-14 21:27:17 +0530

    NEARESTEXIT.cpp
    NEARESTEXIT from SonicWall

---
+++ NEARESTEXIT.cpp
@@ -0,0 +1,35 @@
+
+/* 
+    Dated - 14/09/2022
+    Simple Logical Solution
+    Location BACE Jagatpura
+    Hare Krishna
+    - Prajwal Das
+*/
+
+
+
+#include<iostream>
+
+using namespace std;
+
+int t;
+
+int main() {
+
+    cin >> t;
+    while(t--) {
+        int S;
+        cin >> S;
+        if(S <= 50){
+            cout << "Left" << endl;
+        } else {
+            cout << "Right" << endl;
+        }
+
+
+    }
+
+
+    return 0;
+}
